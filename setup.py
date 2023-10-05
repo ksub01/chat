3abcb2641aab2799d3f284ec14983dd6b8b32063
Danila <k.danila3001@gmail.com>
2023-10-05 07:35:25 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chat_project',
+    version='0.1',
+    packages=['chat', 'chat.migrations', 'chat_project'],
+    url='',
+    license='Apache-2.0 license',
+    author='kuhaku',
+    author_email='k.danila3000@gmail.com',
+    description='Simple chat API'
+)
